a87c7b998472e08c280dd125330a6811521cfb3b
Chase LeCroy <clecroy@bren.ucsb.edu>
2022-02-22 10:48:45 -0800

    trimming example posts, starting to customize

--- packages_script.R
+++ packages_script.R
@@ -2,3 +2,13 @@
 library(blogdown)
 library(pacman)
 library(BiocManager)
+
+#Bash commands to connect local directory and version control repo
+
+# cd <local directory of R project>
+# git init
+# git add.
+# git commit - m "first commit message"
+# git branch -M origin
+# git remote add origin <link to repo>
+# git push -u origin main
